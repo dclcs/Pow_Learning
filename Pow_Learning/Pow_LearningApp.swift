598d10abe603ff38642adb811915425a577d95d6
daicl <daicl@rd.netease.com>
2024-08-20 23:39:51 +0800

    init

---
+++ Pow_Learning/Pow_LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pow_LearningApp.swift
+//  Pow_Learning
+//
+//  Created by daicanglan on 2024/8/20.
+//
+
+import SwiftUI
+
+@main
+struct Pow_LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pow_Learning/Sources/Pow/Infrastructure/AnyContinuousEffect.swift
@@ -0,0 +1,8 @@
+//
+//  AnyContinuousEffect.swift
+//  Pow_Learning
+//
+//  Created by daicanglan on 2024/8/20.
+//
+
+import Foundation
